851c642f77487f2047a47d4b60c91b895d15469a
Felipe Almeida <felipeortalmeida@gmail.com>
2021-03-29 18:58:50 -0300

    feat: bootstrapped home screen

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import {Button, Text} from 'react-native';
+import {Alert, Button, View} from 'react-native';
+import type {ViewStyle} from 'react-native';
 
-import {Screen} from '../../components';
+import {Screen, Text} from '../../components';
 import type {HomeScreenNavigationProps} from '../../@types';
 
 type Props = HomeScreenNavigationProps;
 
+const buttonContainer: ViewStyle = {marginVertical: 16};
+const buttonRow: ViewStyle = {marginBottom: 8};
+const buttonRowLast: ViewStyle = {marginBottom: 0};
+
 export const HomeScreen = ({navigation}: Props) => {
   const goToUserDetail = () => navigation.navigate('UserDetail');
+  const goToUserList = () => Alert.alert('Error', 'Not implemented.');
 
   return (
     <Screen scroll>
-      <Text>HomeScreen</Text>
-      <Button title="Create User" onPress={goToUserDetail} />
+      <Text variant="header">Users</Text>
+      <View style={buttonContainer}>
+        <View style={buttonRow}>
+          <Button title="Create User" onPress={goToUserDetail} />
+        </View>
+        <View style={buttonRowLast}>
+          <Button title="List Users" onPress={goToUserList} />
+        </View>
+      </View>
     </Screen>
   );
 };
