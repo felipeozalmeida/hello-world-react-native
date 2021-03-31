c389195bdb10aa3d7fd3fda7e13fc09622492cee
Felipe Almeida <felipeortalmeida@gmail.com>
2021-03-30 21:56:20 -0300

    feat: used new container on home screen

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import {Alert, Button, View} from 'react-native';
-import type {ViewStyle} from 'react-native';
+import {Alert, Button} from 'react-native';
 
-import {Screen, Text} from '../../components';
+import {ButtonContainer, Screen, Text} from '../../components';
 import type {HomeScreenNavigationProps} from '../../@types';
 
 type Props = HomeScreenNavigationProps;
-
-const buttonContainer: ViewStyle = {marginVertical: 16};
-const buttonRow: ViewStyle = {marginBottom: 8};
-const buttonRowLast: ViewStyle = {marginBottom: 0};
 
 export const HomeScreen = ({navigation}: Props) => {
   const goToUserDetail = () => navigation.navigate('UserDetail');
@@ -18,14 +13,10 @@ export const HomeScreen = ({navigation}: Props) => {
   return (
     <Screen scroll>
       <Text variant="header">Users</Text>
-      <View style={buttonContainer}>
-        <View style={buttonRow}>
-          <Button title="Create User" onPress={goToUserDetail} />
-        </View>
-        <View style={buttonRowLast}>
-          <Button title="List Users" onPress={goToUserList} />
-        </View>
-      </View>
+      <ButtonContainer>
+        <Button title="Create User" onPress={goToUserDetail} />
+        <Button title="List Users" onPress={goToUserList} />
+      </ButtonContainer>
     </Screen>
   );
 };
