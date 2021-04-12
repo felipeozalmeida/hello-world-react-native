309d238da61108fdf3e01d1ca6216c3f9d7dff25
Felipe Almeida <felipeortalmeida@gmail.com>
2021-04-12 18:16:12 -0300

    feat: added watermelondb

---
+++ src/storage/database.ts
@@ -0,0 +1,17 @@
+import {Database} from '@nozbe/watermelondb';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+
+import schema from './schema';
+// import Post from './Post' // ⬅️ You'll import your Models here
+
+// First, create the adapter to the underlying database:
+const adapter = new SQLiteAdapter({schema});
+
+// Then, make a Watermelon database from it!
+export const database = new Database({
+  adapter,
+  modelClasses: [
+    // Post, // ⬅️ You'll add Models to Watermelon here
+  ],
+  actionsEnabled: true,
+});
