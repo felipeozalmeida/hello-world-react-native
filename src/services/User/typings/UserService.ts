a6472ad55e2730f5b9e31484dfbca00825e667bf
Felipe Almeida <felipeortalmeida@gmail.com>
2021-04-26 19:01:08 -0300

    refactor: reordered type

--- src/services/User/typings/UserService.ts
+++ src/services/User/typings/UserService.ts
@@ -1,8 +1,8 @@
 import type {User, UserWithoutId} from '../../../models';
 
 export type UserService = {
-  get: (id: User['id']) => Promise<User>;
   list: () => Promise<User[]>;
+  get: (id: User['id']) => Promise<User>;
   create: (user: UserWithoutId) => Promise<User>;
   update: (user: User) => Promise<User>;
   delete: (id: User['id']) => Promise<void>;
